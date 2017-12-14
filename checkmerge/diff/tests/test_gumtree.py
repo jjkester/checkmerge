33f508bb9088f1ccceaea65962e20e1c5ba12efe
Jan-Jelle Kester <janjelle@jjkester.nl>
2017-12-14 16:27:07 +0100

    For now only use GumTree unit tests ported from the Java implementation

--- checkmerge/diff/tests/test_gumtree.py
+++ checkmerge/diff/tests/test_gumtree.py
@@ -1,173 +1,56 @@
 import unittest
-from copy import deepcopy
 
 from checkmerge.diff.gumtree import GumTreeDiff
 from checkmerge.ir.tree import IRNode
 
 
-class GumTreeTestCase(unittest.TestCase):
+class OriginalGumTreeTestCase(unittest.TestCase):
+    """
+    Test case for the GumTree algorithm implementation that was taken from the original Java implementation of GumTree.
+    """
     def setUp(self):
-        self.t1 = IRNode("Sequence", children=[
-            IRNode("While", children=[
-                IRNode("Operator", "!=", children=[
-                    IRNode("Variable", "b"),
-                    IRNode("Value", "0"),
-                ]),
-                IRNode("Conditional", children=[
-                    IRNode("Operator", ">", children=[
-                        IRNode("Variable", "a"),
-                        IRNode("Variable", "b"),
-                    ]),
-                    IRNode("Assignment", children=[
-                        IRNode("Variable", "a"),
-                        IRNode("Operator", "-", children=[
-                            IRNode("Variable", "a"),
-                            IRNode("Variable", "b"),
-                        ]),
-                    ]),
-                    IRNode("Assignment", children=[
-                        IRNode("Variable", "b"),
-                        IRNode("Operator", "-", children=[
-                            IRNode("Variable", "b"),
-                            IRNode("Variable", "a"),
-                        ]),
-                    ]),
-                ]),
+        self.t1 = IRNode(typ="0", label="a", children=[
+            IRNode(typ="0", label="e", children=[
+                IRNode(typ="2", label="f"),
             ]),
-            IRNode("Return", children=[
-                IRNode("Variable", "a"),
+            IRNode(typ="0", label="b", children=[
+                IRNode(typ="0", label="c"),
+                IRNode(typ="0", label="d"),
             ]),
+            IRNode(typ="0", label="g"),
         ])
 
-        self.t2 = IRNode("Sequence", children=[
-            IRNode("While", children=[
-                IRNode("Operator", "!=", children=[
-                    IRNode("Variable", "b"),
-                    IRNode("Value", "0"),
-                ]),
-                IRNode("Conditional", children=[
-                    IRNode("Operator", "<", children=[
-                        IRNode("Variable", "a"),
-                        IRNode("Variable", "b"),
-                    ]),
-                    IRNode("Assignment", children=[
-                        IRNode("Variable", "a"),
-                        IRNode("Operator", "-", children=[
-                            IRNode("Variable", "a"),
-                            IRNode("Variable", "b"),
-                        ]),
-                    ]),
-                    IRNode("Assignment", children=[
-                        IRNode("Variable", "b"),
-                        IRNode("Operator", "-", children=[
-                            IRNode("Variable", "b"),
-                            IRNode("Variable", "a"),
-                        ]),
-                    ]),
+        self.t2 = IRNode(typ="0", label="z", children=[
+            IRNode(typ="0", label="b", children=[
+                IRNode(typ="0", label="c"),
+                IRNode(typ="0", label="d"),
+            ]),
+            IRNode(typ="1", label="h", children=[
+                IRNode(typ="0", label="e", children=[
+                    IRNode(typ="2", label="y"),
                 ]),
             ]),
-            IRNode("Return", children=[
-                IRNode("Operator", "+", children=[
-                    IRNode("Value", "1"),
-                    IRNode("Variable", "a"),
-                ]),
-            ]),
+            IRNode(typ="0", label="g"),
         ])
 
-    def test_top_down_equal(self):
-        """Tests the top down phase algorithm with two equal trees."""
-        diff = GumTreeDiff()
-        t1 = self.t1
-        t2 = deepcopy(self.t1)
+    def test_min_height_threshold(self):
+        diff = GumTreeDiff(min_height=0, max_size=0)
+        mapping = diff(self.t1, self.t2)
 
-        mapping = diff.top_down(t1, t2)
+        self.assertEqual(5, len(mapping))
 
-        self.assertEqual(21, len(mapping))
-        self.assertEqual(t2, mapping[t1])
+        diff = GumTreeDiff(min_height=1, max_size=0)
+        mapping = diff(self.t1, self.t2)
 
-        t1_nodes = list(t1.descendants)
-        t2_nodes = list(t2.descendants)
+        self.assertEqual(4, len(mapping))
 
-        self.assertTrue(len(t1_nodes) == len(t2_nodes))
+    def test_max_size_threshold(self):
+        diff = GumTreeDiff(min_height=0, max_size=0)
+        mapping = diff(self.t1, self.t2)
 
-        # Test if the nodes are mapped correctly
-        for i in range(0, len(t1_nodes)):
-            self.assertEqual(t2_nodes[i].hash, mapping[t1_nodes[i]].hash)
-            self.assertEqual(t2_nodes[i].type, mapping[t1_nodes[i]].type)
-            self.assertEqual(t2_nodes[i].label, mapping[t1_nodes[i]].label)
-            self.assertEqual(len(t2_nodes[i].children), len(mapping[t1_nodes[i]].children))
+        self.assertEqual(5, len(mapping))
 
-    def test_top_down_unequal(self):
-        """Tests the top down phase algorithm with two slightly different trees."""
-        diff = GumTreeDiff()
-        t1 = self.t1
-        t2 = self.t2
+        diff = GumTreeDiff(min_height=0, max_size=8)
+        mapping = diff(self.t1, self.t2)
 
-        mapping = diff.top_down(t1, t2)
-
-        expected_mapping = {
-            t1[0][1][1]: t2[0][1][1],
-            t1[0][1][1][0]: t2[0][1][1][0],
-            t1[0][1][1][1]: t2[0][1][1][1],
-            t1[0][1][1][1][0]: t2[0][1][1][1][0],
-            t1[0][1][1][1][1]: t2[0][1][1][1][1],
-            t1[0][1][2]: t2[0][1][2],
-            t1[0][1][2][0]: t2[0][1][2][0],
-            t1[0][1][2][1]: t2[0][1][2][1],
-            t1[0][1][2][1][0]: t2[0][1][2][1][0],
-            t1[0][1][2][1][1]: t2[0][1][2][1][1],
-        }
-
-        self.assertEqual(len(expected_mapping), len(mapping))
-
-        hash_mapping = {k.hash: v.hash for k, v in mapping.items()}
-        expected_hash_mapping = {k.hash: v.hash for k, v in expected_mapping.items()}
-
-        self.assertEqual(expected_hash_mapping, hash_mapping)
-
-    def test_complete_equal(self):
-        """Tests the GumTree algorithm with two equal trees."""
-        diff = GumTreeDiff()
-        t1 = self.t1
-        t2 = deepcopy(self.t1)
-
-        mapping = diff(t1, t2)
-
-        self.assertEqual(21, len(mapping))
-        self.assertEqual(t2, mapping[t1])
-
-        t1_nodes = list(t1.descendants)
-        t2_nodes = list(t2.descendants)
-
-        self.assertTrue(len(t1_nodes) == len(t2_nodes))
-
-        # Test if the nodes are mapped correctly
-        for i in range(0, len(t1_nodes)):
-            self.assertEqual(t2_nodes[i].hash, mapping[t1_nodes[i]].hash)
-            self.assertEqual(t2_nodes[i].type, mapping[t1_nodes[i]].type)
-            self.assertEqual(t2_nodes[i].label, mapping[t1_nodes[i]].label)
-            self.assertEqual(len(t2_nodes[i].children), len(mapping[t1_nodes[i]].children))
-
-    def test_complete_unequal(self):
-        """Tests the GumTree algorithm with two slightly different trees."""
-        diff = GumTreeDiff()
-        t1 = self.t1
-        t2 = deepcopy(self.t1)
-
-        # Change label to trigger difference
-        t2.label = "(different)"
-
-        mapping = diff(t1, t2)
-
-        t1_nodes = list(t1.nodes)
-        t2_nodes = list(t2.nodes)
-
-        expected_mapping = {t1_nodes[i]: t2_nodes[i] for i in range(len(t1_nodes))}
-        # Remove "Return" subtree - not matched due to height of 2
-        del expected_mapping[t1[1]]
-        del expected_mapping[t1[1][0]]
-
-        hash_mapping = {k.hash: v.hash for k, v in mapping.items()}
-        expected_hash_mapping = {k.hash: v.hash for k, v in expected_mapping.items()}
-
-        self.assertEqual(expected_hash_mapping, hash_mapping)
+        self.assertEqual(6, len(mapping))
