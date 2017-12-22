30f25eaee9069fc31d5b3d2b4e269f53ef1c5f36
Jan-Jelle Kester <janjelle@jjkester.nl>
2017-12-22 14:02:48 +0100

    Split scope from collections

--- checkmerge/util/collections.py
+++ checkmerge/util/collections.py
@@ -1,5 +1,3 @@
-import abc
-import collections
 import heapq
 import typing
 
@@ -44,68 +42,3 @@ class PriorityList(object):
 
     def __len__(self):
         return len(self.data)
-
-
-class ScopeBase(collections.MutableMapping, metaclass=abc.ABCMeta):
-    """
-    Base class for the Scope collection.
-    """
-    __slots__ = ()
-
-
-class EmptyScope(ScopeBase):
-    """
-    An empty, no-op scope. Used as a (singleton) placeholder for empty parents.
-    """
-    __slots__ = ()
-
-    def __setitem__(self, key, value):
-        raise KeyError(key)
-
-    def __delitem__(self, key):
-        raise KeyError(key)
-
-    def __getitem__(self, key):
-        raise KeyError(key)
-
-    def __len__(self):
-        return 0
-
-    def __iter__(self):
-        yield from ()
-
-
-class Scope(ScopeBase):
-    EMPTY = EmptyScope()
-
-    __slots__ = ('_parent', '_data')
-
-    def __init__(self, parent: ScopeBase = EMPTY):
-        # Check parent for validity
-        if not isinstance(parent, ScopeBase):
-            raise TypeError("Only instances of Scope can be used as parent.")
-
-        # Initialize slots
-        self._data = {}
-        self._parent = parent
-
-    @property
-    def parent(self):
-        return self._parent
-
-    def __getitem__(self, key):
-        if key in self._data:
-            return self._data[key]
-        return self._parent[key]
-
-    def __setitem__(self, key, value):
-        self._data[key] = value
-
-    def __delitem__(self, key):
-        del self._data[key]
-
-    def __len__(self):
-        return len(set().union(self._data, self._parent))
-
-    def __iter__(self):
-        yield from set().union(self._data, self._parent)
--- checkmerge/util/tests/test_collections.py
+++ checkmerge/util/tests/test_collections.py
@@ -1,6 +1,6 @@
 import unittest
 
-from checkmerge.util.collections import PriorityList, Scope
+from checkmerge.util.collections import PriorityList
 
 
 class PriorityListTestCase(unittest.TestCase):
@@ -52,92 +52,3 @@ class PriorityListTestCase(unittest.TestCase):
         self.assertEqual(-1, pl.pop())
         self.assertFalse(pl)
         self.assertEqual(0, len(pl))
-
-
-class ScopeTestCase(unittest.TestCase):
-    """
-    Test case for the Scope data structure.
-    """
-    def setUp(self):
-        self.bottom = Scope()
-        self.middle = Scope(parent=self.bottom)
-        self.top = Scope(parent=self.middle)
-
-        self.bottom.update(foo=10, bar=20, baz=30)
-        self.middle.update(bar=21, qux=41)
-        self.top.update(baz=32, quux=52)
-
-        self.scope = self.top
-
-    def test_get(self):
-        """Tests the recursive get method of a Scope."""
-        self.assertEqual(10, self.scope.get('foo'))
-        self.assertEqual(21, self.scope.get('bar'))
-        self.assertEqual(32, self.scope.get('baz'))
-        self.assertEqual(41, self.scope.get('qux'))
-        self.assertEqual(52, self.scope.get('quux'))
-        self.assertEqual(None, self.scope.get('quuux'))
-        self.assertEqual(10, self.scope.get('foo', default=9))
-        self.assertEqual(9, self.scope.get('quuux', default=9))
-
-        self.assertEqual(30, self.middle.get('baz'))
-        self.assertEqual(None, self.bottom.get('qux'))
-
-        self.assertEqual(10, self.scope['foo'])
-        self.assertEqual(21, self.scope['bar'])
-        self.assertEqual(32, self.scope['baz'])
-        self.assertEqual(41, self.scope['qux'])
-
-        try:
-            self.scope['quuux']
-        except KeyError:
-            pass
-        else:
-            self.fail()
-
-    def test_set(self):
-        """Tests setting items on a scope."""
-        self.scope['foo'] = 13
-        self.scope.update(qux=43)
-
-        self.assertEqual(13, self.scope['foo'])
-        self.assertEqual(43, self.scope['qux'])
-        self.assertEqual(10, self.middle['foo'])
-        self.assertEqual(41, self.middle['qux'])
-
-    def test_del(self):
-        """Tests deleting items from a scope."""
-        del self.scope['baz']
-
-        # Expected KeyError when deleting bar, fail if no error
-        try:
-            del self.scope['bar']
-        except KeyError:
-            pass
-        else:
-            self.fail()
-
-        self.assertEqual(30, self.scope.get('baz'))
-        self.assertEqual(21, self.scope.get('bar'))
-
-    def test_len(self):
-        """Tests the length of a scope."""
-        self.assertEqual(3, len(self.bottom))
-        self.assertEqual(4, len(self.middle))
-        self.assertEqual(5, len(self.top))
-
-        self.scope['quuux'] = 63
-
-        self.assertEqual(3, len(self.bottom))
-        self.assertEqual(4, len(self.middle))
-        self.assertEqual(6, len(self.top))
-
-    def test_keys(self):
-        """Tests the keys of a scpe."""
-        self.assertEqual({'foo', 'bar', 'baz'}, self.bottom.keys())
-        self.assertEqual({'foo', 'bar', 'baz', 'qux'}, self.middle.keys())
-        self.assertEqual({'foo', 'bar', 'baz', 'qux', 'quux'}, self.top.keys())
-
-    def test_immutable_mapping(self):
-        """Tests the immutable mapping characteristics of a Scope."""
-
