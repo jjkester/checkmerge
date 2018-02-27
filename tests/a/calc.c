7c090a37d0e61f349a5606d058dc967e2cccb7b7
Jan-Jelle Kester <janjelle@jjkester.nl>
2018-02-27 14:54:41 +0100

    Go back to original calc.c test

--- tests/a/calc.c
+++ tests/a/calc.c
@@ -1,11 +1,7 @@
-//#include <stdio.h>
+#include <stdio.h>
 
 int calc(int a, int b) {
     int c = a + b - 1;
     printf("c=%d\n", c);
     return c;
 }
-
-//int main() {
-//    return calc(1,1);
-//}
--- tests/b/calc.c
+++ tests/b/calc.c
@@ -1,11 +1,7 @@
-//#include <stdio.h>
+#include <stdio.h>
 
 int calc(int a, int b) {
     int c = a + b;
     printf("c=%d\n", c);
     return c - 1;
 }
-
-//int main() {
-//    return calc(1,1);
-//}
